f62fa52eb5f37ec94a8b31b2651dfbe3ba038633
Spencman101 <rey23026@byui.edu>
2024-10-29 22:33:41 -0600

    Image to Webp Changes

--- second-chance/main.js
+++ second-chance/main.js
@@ -1,33 +1,33 @@
 const rebuilds = [
     {
         id: 1,
-        beforeSrc: 'images/vehicle/bayou5.jpg',
+        beforeSrc: 'images/vehicle/bayou5.webp',
         beforeAlt: 'bayou_before',
-        afterSrc: 'images/vehicle/bayou1.jpeg',
+        afterSrc: 'images/vehicle/bayou1.webp',
         afterAlt: 'bayou_after',
         description: "While our family was living in Louisiana Covid hit and we needed something to do with our time. Dad decided to buy a salvaged jeep and rebuild it as a family. Through this project, I watched him teach our kids about cars, tools, and the processes of taking something old and broken and giving it a second chance. My favorite part was watching my children and husband laugh and smile while strengthening their relationships with each other."
     },
     {
         id: 2,
-        beforeSrc: 'images/vehicle/karma2.jpg',
+        beforeSrc: 'images/vehicle/karma2.webp',
         beforeAlt: 'karma_before',
-        afterSrc: 'images/vehicle/karma3.jpg',
+        afterSrc: 'images/vehicle/karma3.webp',
         afterAlt: 'karma_after',
         description: ""
     },
     {
         id: 3,
-        beforeSrc: 'images/vehicle/sabotage2.jpg',
+        beforeSrc: 'images/vehicle/sabotage2.webp',
         beforeAlt: 'sabotage_before',
-        afterSrc: 'images/vehicle/sabotage3.jpg',
+        afterSrc: 'images/vehicle/sabotage3.webp',
         afterAlt: 'sabotage_after',
         description: "I was able to build and design my dream Jeep with my dad. It was a fun process learning more about cars and how to fix them. Being able to be a part of the whole process was such a wonderful experience. My blood, sweat, and tears went into this project, but I wouldn't have it any other way. It gave me the opportunity to learn and grow and I wouldn't change this experience for the world."
     },
     {
         id: 4,
-        beforeSrc: 'images/vehicle/solstice1.jpg',
+        beforeSrc: 'images/vehicle/solstice1.webp',
         beforeAlt: 'solstice_before',
-        afterSrc: 'images/vehicle/solstice2.jpg',
+        afterSrc: 'images/vehicle/solstice2.webp',
         afterAlt: 'solstice_after',
         description: ''
     },
